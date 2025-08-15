eb3fde541cf6439e791a1d9376187c126d8c2aa4
Abdullah Rabi <abdullahrabi026@gmail.com>
2025-08-16 01:50:33 +0500

    fixing

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 4000;
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(bodyParser.json());
 await connectDb()
 
 app.use('/api/user', userRouter)
